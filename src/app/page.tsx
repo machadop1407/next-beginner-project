581330202b0c25a415afaa8365b3ea0a51051b7d
kisuvilu <kisuvilu@gmail.com>
2024-03-09 19:11:20 +0300

    Client page files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,25 @@
-"use client";
-import { useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
-
+"use client"
+import {useState, FormEvent } from 'react'; 
 export default function Home() {
-  const [value, setValue] = useState("");
-  const { push } = useRouter();
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    push(`/prediction/${value}`);
-  };
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="p-4 shadow-md bg-white rounded-md">
-        <h1 className="text-2xl font-semibold mb-4 text-black">
-          Enter Your Name
-        </h1>
-        <form onSubmit={handleSubmit} className="space-y-3">
-          <input
-            type="text"
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            className="w-full p-2 border border-gray-300 rounded text-black"
-            placeholder="Type something..."
-          />
-          <button
-            type="submit"
-            className="w-full py-2 px-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
+    const [inputVal, setInputVal] = useState("");
+    const handlesubmit = (event: FormEvent) => {
+     
+        
+    };
+    return ( 
+    <div>
+       <div>
+        <h1>Enter Your Name </h1>
+       </div>
+       <form onsubmit={handlesubmit}> 
+         <input 
+           type="text" 
+           placeholder="Enter Your Name.."  
+           onChange={(e) => setInputVal(e.target.value)}
+           />
+         <button type="submit"> Predict Data </button>
+       </form>
     </div>
-  );
-}
+    );
+    
+  }
