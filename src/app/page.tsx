6fb327bc6ef13f60080c970750566ddd50306652
kisuvilu <kisuvilu@gmail.com>
2024-03-09 20:24:44 +0300

    routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,32 @@
 "use client"
-import {useState, FormEvent } from 'react'; 
+import { useState, FormEvent } from 'react'; 
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
-    const [inputVal, setInputVal] = useState("");
-    const handlesubmit = (event: FormEvent) => {
-     
-        
-    };
-    return ( 
+  const [inputVal, setInputVal] = useState("");
+  const { push } = useRouter();
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    push("/prediction/" + inputVal);
+  };
+
+  return ( 
     <div>
-       <div>
+      <div>
         <h1>Enter Your Name </h1>
-       </div>
-       <form onsubmit={handlesubmit}> 
-         <input 
-           type="text" 
-           placeholder="Enter Your Name.."  
-           onChange={(e) => setInputVal(e.target.value)}
-           />
-         <button type="submit"> Predict Data </button>
-       </form>
+      </div>
+      <form onSubmit={handleSubmit}> {/* Corrected 'onsubmit' to 'onSubmit' */}
+        <input 
+          type="text" 
+          placeholder="Enter Your Name.." 
+          value={inputVal}
+          className='text-black' 
+          onChange={(e) => setInputVal(e.target.value)}
+        />
+        <button type="submit">Predict Data</button>
+      </form>
     </div>
-    );
-    
-  }
+  );
+}
+  
--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -1,6 +1,36 @@
-export default function Home() {
+const getPredictionAge = async (name: string) => {
+  const res = await fetch(`http://api.agify.io/?name=${name}`);
+  return res.json();
+};
+
+const getPredictionGender = async (name: string) => {
+  const res = await fetch(`http://api.genderize.io/?name=${name}`);
+  return res.json();
+};
+
+const getPredictionCountry = async (name: string) => {
+  const res = await fetch(`http://api.nationalize.io/?name=${name}`);
+  return res.json();
+};
+
+interface Params {
+  params: {
+    name: string;
+  };
+}
+
+export default async function Page({ params }: Params) {
+  const [ageData, genderData, countryData] = await Promise.all([
+    getPredictionAge(params.name),
+    getPredictionGender(params.name),
+    getPredictionCountry(params.name),
+  ]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  )
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Age: {ageData.age}</h1>
+      <h1>Gender: {genderData.gender}</h1>
+      <h1>Country: {countryData.country}</h1>
+    </main>
   );
 }
